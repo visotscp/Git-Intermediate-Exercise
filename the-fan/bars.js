af9ec614c10de0040de582be941d814de762b8bf
Rob Shircliff <rshircliff@gmail.com>
2018-05-21 12:37:17 -0400

    Add data for The Fan establishments
    Adds array of JSON objects that contains information
    about neighborhood establishments in The Fan this data has been
     -researched, and
     -syntatically validated
    
    Fixes #14.

--- the-fan/bars.js
+++ the-fan/bars.js
@@ -1,6 +1,31 @@
 var document = "";
 
-var data;
+var data = [
+  {
+    "name":"The Jasper",
+    "neighborhood":"The Fan",
+    "address":"3113 W Cary St",
+    "note":"Fast Service, Great cocktails"
+  },
+  {
+    "name":"City Beach",
+    "neighborhood":"The Fan",
+    "address":"2416 W Cary St",
+    "note":"very spacious, great drinks"
+  },
+  {
+    "name":"Canon & Draw Brewing",
+    "neighborhood":"The Fan",
+    "address":"1529 W Main St",
+    "note":"Cool spot!"
+  },
+  {
+    "name":"social52",
+    "neighborhood":"The Fan",
+    "address":"2619 W Main St",
+    "note":"Great Atmosphere!"
+  }
+];
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
