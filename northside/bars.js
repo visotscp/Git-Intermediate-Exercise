053adf7f1b4a488ecae2eb54bcbf3f2deff56543
Maggie Cosby <maggie_cosby@carmax.com>
2018-05-21 12:43:51 -0400

    Add bars for Northside

--- northside/bars.js
+++ northside/bars.js
@@ -1,6 +1,31 @@
 var document = "";
 
-var data;
+var data = [
+  {
+    "name": "Boogaloos Bar and Grill",
+    "neighborhood": "Northside",
+    "address": "2602, 210 W Brookland Park Blvd, Richmond, VA 23222",
+    "note": "Cajun and creole cuisine and drink specials"
+  },
+  {
+    "name": "J.R. Crickets RVA",
+    "neighborhood": "Northside",
+    "address": "1509 Chamberlayne Ave, Richmond, VA 23222",
+    "note": "Sports bar with wings"
+  },
+  {
+    "name": "Bryan Park Bar and Grill",
+    "neighborhood": "Northside",
+    "address": "5516 Lakeside Ave, Richmond, VA 23228",
+    "note": "Happy Hour"
+  },
+  {
+    "name": "Lakeside Tavern",
+    "neighborhood": "Northside",
+    "address": "5406 Lakeside Ave, Henrico, VA 23228",
+    "note": "Karaoke"
+  }
+]
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
