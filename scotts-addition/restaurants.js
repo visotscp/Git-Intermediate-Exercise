75f9da1cea719a166fca38bb8b0df6dd3bd91c87
Joseph Gay <josephmgay2@gmail.com>
2018-05-21 13:05:43 -0400

    Added periods to notes section

--- scotts-addition/restaurants.js
+++ scotts-addition/restaurants.js
@@ -5,31 +5,31 @@ var data = [
     "name": "Peter Chang",
     "neighborhood": "Scott's Addition",
     "address": "2816 W Broad St",
-    "note": "Hip spot for contemporary chinese eats"
+    "note": "Hip spot for contemporary chinese eats."
   },
   {
     "name": "Buz and Ned's",
     "neighborhood": "Scott's Addition",
     "address": "1999 N Boulevard",
-    "note": "Smokehouse mainstay with Microbrews"
+    "note": "Smokehouse mainstay with Microbrews."
   },
   {
     "name": "Sabai",
     "neighborhood": "Scott's Addition",
     "address": "2727 W Broad St",
-    "note": "Thai street food in a funky bar/eatery"
+    "note": "Thai street food in a funky bar/eatery."
   },
   {
     "name": "En Su Boca",
     "neighborhood": "Scott's Addition",
     "address": "1001 N Boulevard",
-    "note": "Tex-Mex plates & drinks until late"
+    "note": "Tex-Mex plates & drinks until late."
   },
   {
     "name": "Stella's",
     "neighborhood": "Scott's Addition",
     "address": "1012 Lafayette St",
-    "note": "Greek bistro with modern eats & decor"
+    "note": "Greek bistro with modern eats & decor."
   }
 ];
 
