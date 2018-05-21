e13d82da257aef6b95f41a977f67cba76e7e91a5
David Stone <stone.david44@gmail.com>
2018-05-21 12:47:56 -0400

    added Michael's space to church Hill fixes # 13

--- church-hill/bars.js
+++ church-hill/bars.js
@@ -3,25 +3,25 @@ var document = "";
 var data = [
 {
 	"name":"Metzger Bar and Butchery",
-	"neighborhood":"ChurchHill",
+	"neighborhood":"Church Hill",
 	"address":"801 N 23rd St",
 	"note":"Rustic-chic venture offering seasonal & sustainable German-inspired plates, plus wines by the glass"
 	},
 	{
 	"name":"Patrick Henry's Pub and Grill",
-	"neighborhood":"ChurchHill",
+	"neighborhood":"Church Hill",
 	"address":"2300 E Broad St",
 	"note":"Cozy pub in the historical Patrick Henry Inn offering American grub, draft beer & a charming patio."
 	},
 	{
 	"name":"Poe's Pub",
-	"neighborhood":"ChurchHill",
+	"neighborhood":"Church Hill",
 	"address":"2706 E Main St",
 	"note":"Live blues & Americana meet Southern eats at this homey pub in a white cottage hung with photos"
 	},
 	{
 	"name":"7 Hills Brewing Company",
-	"neighborhood":"ChurchHill",
+	"neighborhood":"Church Hill",
 	"address":"115 S 15th St #100",
 	"note":"Energetic gastropub featuring surf and turf & its own craft brews, plus growlers to go & a patio."
 	}
