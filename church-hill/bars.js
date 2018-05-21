90cc4df260cc4660b75dd09af98acb60a47374f0
David Stone <stone.david44@gmail.com>
2018-05-21 12:02:08 -0400

    Added data for Church Hill Bars and Breweries
    ADded an array of Bars for Church Hill.
    Fixes # 13

--- church-hill/bars.js
+++ church-hill/bars.js
@@ -1,6 +1,31 @@
 var document = "";
 
-var data;
+var data = [
+{
+	"name":"Metzger Bar and Butchery",
+	"neighborhood":"ChurchHill",
+	"address":"801 N 23rd St",
+	"note":"Rustic-chic venture offering seasonal & sustainable German-inspired plates, plus wines by the glass"
+	},
+	{
+	"name":"Patrick Henry's Pub and Grill",
+	"neighborhood":"ChurchHill",
+	"address":"2300 E Broad St",
+	"note":"Cozy pub in the historical Patrick Henry Inn offering American grub, draft beer & a charming patio."
+	},
+	{
+	"name":"Poe's Pub",
+	"neighborhood":"ChurchHill",
+	"address":"2706 E Main St",
+	"note":"Live blues & Americana meet Southern eats at this homey pub in a white cottage hung with photos"
+	},
+	{
+	"name":"7 Hills Brewing Company",
+	"neighborhood":"ChurchHill",
+	"address":"115 S 15th St #100",
+	"note":"Energetic gastropub featuring surf and turf & its own craft brews, plus growlers to go & a patio."
+	}
+];
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
