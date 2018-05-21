ecb3ad9b1ccf9af9ecfae4f5566f268d48d3de33
Sandeep Dange <Sandeep_dange@hotmail.com>
2018-05-21 12:15:39 -0400

    Added Restaurants in Shockoe Bottom in JSON format

--- shockoe-bottom/restaurants.js
+++ shockoe-bottom/restaurants.js
@@ -1,6 +1,31 @@
 var document = "";
 
-var data;
+var data = [
+  {
+		"name": "Millie’s",
+		"neighborhood": "Shockoe Bottom",
+		"address": "2603 E Main St",
+		"note": "$$  American (New), Breakfast & Brunch, Bars"
+	},
+	{
+		"name": "LuLu’s",
+		"neighborhood": "Shockoe Bottom",
+		"address": "21 N 17th St",
+		"note": "$$  American (New), Breakfast & Brunch, Bars"
+	},
+	{
+		"name": "Bottoms Up Pizza",
+		"neighborhood": "Shockoe Bottom",
+		"address": "1700 Dock St",
+		"note": "$$  Pizza"
+	},
+	{
+		"name": "The Boathouse at Rocketts Landing",
+		"neighborhood": "Shockoe Bottom",
+		"address": "4708 E Old Main St",
+		"note": "$$$  Seafood, Venues & Event Spaces, Bars"
+	}
+];
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
