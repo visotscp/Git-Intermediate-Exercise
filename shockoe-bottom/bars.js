0a005ce449790ccc899fae88baf55cc525296f3a
Scott Byrd <sbyrd1968@verizon.net>
2018-05-21 12:42:27 -0400

    Add data for <BARS and BREWERIES> establishments Adds an array of JSON objects that contains information about neighborhood establishments in <NEIGHBORHOOD>. This data has been - researched, and - syntatically validated Fixes #06.

--- shockoe-bottom/bars.js
+++ shockoe-bottom/bars.js
@@ -4,6 +4,32 @@ var data;
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
+  var data = [
+    {
+    "name": "Stone Brewing",
+    "neighborhood": "Shockoe Bottom",
+    "address": "4300 Williamsburg Ave",
+    "note": "Beautiful place. Easy to get to. Large selection of beers."  
+    },
+    {
+    "name": "Lickinghole Creek Brew Pub",
+    "neighborhood": "Shockoe Bottom",
+    "address": "1717 E. Franklin Street",
+    "note": "8 beers on tap. Nice place. Solid beers brewed at Goochlnad location."
+    },
+    {
+      "name": "Ponies & Pints",
+      "neighborhood": "Shockoe Bottom",
+      "address": "110 N. 18th Street",
+      "note": "Bet on the ponies and get something to eat and drink. 11AM Happy Hour!"
+    },
+    {
+      "name": "Nota Bene Restaurant & Bar",
+      "neighborhood": "Shockoe Bottom",
+      "address": "2110 E. Main Street",
+      "note": "Dimly lit restaurant and bar with great food."
+    }
+  ];
   console.group("Suggestion");
   console.log(JSON.stringify(suggestion, null, '  '));
   console.groupEnd();
