b5bd5f04bd0d509836675a9918f9a825995a1889
Joseph Gay <josephmgay2@gmail.com>
2018-05-21 12:07:14 -0400

    Added list of restaurants to Scott's Addition JS file

--- scotts-addition/restaurants.js
+++ scotts-addition/restaurants.js
@@ -1,6 +1,37 @@
 var document = "";
 
-var data;
+var data = [
+  {
+    "name": "Peter Chang",
+    "neighborhood": "Scott's Addition",
+    "address": "2816 W Broad St",
+    "note": "Hip spot for contemporary chinese eats"
+  },
+  {
+    "name": "Buz and Ned's",
+    "neighborhood": "Scott's Addition",
+    "address": "1999 N Boulevard",
+    "note": "Smokehouse mainstay with Microbrews"
+  },
+  {
+    "name": "Sabai",
+    "neighborhood": "Scott's Addition",
+    "address": "2727 W Broad St",
+    "note": "Thai street food in a funky bar/eatery"
+  },
+  {
+    "name": "En Su Boca",
+    "neighborhood": "Scott's Addition",
+    "address": "1001 N Boulevard",
+    "note": "Tex-Mex plates & drinks until late"
+  },
+  {
+    "name": "Stella's",
+    "neighborhood": "Scott's Addition",
+    "address": "1012 Lafayette St",
+    "note": "Greek bistro with modern eats & decor"
+  }
+];
 
 function printSuggestion(inputData) {
   var suggestion = inputData[Math.floor(Math.random() * inputData.length)];
